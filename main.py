4fe6cfff9dc4656cef788d16311185cd64bd5211
Aeggne Miranda <85737157+kabligablonk@users.noreply.github.com>
2024-11-07 01:19:58 +0000

    Made raspberry compatible with main

--- main.py
+++ main.py
@@ -1,13 +1,13 @@
 import Data
+import raspberry as rasp
 import PySimpleGUI as sg
 
 sg.theme('Dark Brown 1')
 information = Data.information 
 
-
 # Returning info depending on button increment/decrement
 def get_info(index):
-    keys = list(information.keys())
+    keys = list(information.keys()) # Turn key names into natural language numbers
     if index < len(keys):
         key = keys[index]
         try:
@@ -15,8 +15,7 @@ def get_info(index):
         except Exception:
             return information[key]
     else:
-        return None
-
+        return "Index out of range"
 
 # GUI layout
 layout = [
@@ -25,28 +24,31 @@ layout = [
     [sg.Button("Get Info"), sg.Button("Exit")]
 ]
 
-tracker_layout = [
-    # Add layout
+# GUI for tracker window
+tracker_window = [
+    [sg.Text("Information Tracker")],
+    [sg.Text("Index: "), sg.Text(rasp.dictionary_index, key='-INDEX-')]
 ]
 
 # Create the window
 window = sg.Window("Group 3 ACEBIO", layout, size=(700, 400))
+tracker_window = sg.Window("Information Tracker", tracker_window, size=(200,400))
 
 while True:
-    event, values = window.read()
+    event, values = window.read(timeout=100)  # Add timeout to periodically check for updates
     if event == sg.WIN_CLOSED or event == "Exit":
         break
-    elif event == "Get Info":
+    elif event == "Get Info" or rasp.submit_pressed:
         try:
             index = int(values['-INPUT-'])
             result = get_info(index)
             window['-RESULT-'].update(result)
+            window['-INPUT-'].update("")  # Clear the input field after getting info
+            rasp.submit_pressed = False  # Reset the submit button state to false
         except ValueError as e:
             sg.popup_error(f"Enter a valid index. \n Error: {e}")
 
-
-
-# Create second window for info tracker
-tracker_window = sg.Window("Tracked Information", tracker_layout, size=(100, 400))
+    # Update the index number based on button pressed
+    window['-INPUT-'].update(rasp.dictionary_index)
 
 window.close()
--- raspberry.py
+++ raspberry.py
@@ -3,21 +3,35 @@ import time
 
 # note to me: put the LEDs on pinout 17, 27, 22
 GPIO.setmode(GPIO.BCM)
-GPIO.setup(17, GPIO.IN, pull_up_dow=GPIO.PUD_UP)
-GPIO.setup(27, GPIO.IN, pull_up_dow=GPIO.PUD_UP)
-GPIO.setup(22, GPIO.IN, pull_up_dow=GPIO.PUD_UP)
+GPIO.setup(17, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+GPIO.setup(27, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+GPIO.setup(22, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 
 # global variable dictionary index
 dictionary_index = 1
-
-if dictionary_index < 1:
-    dictionary_index = 1
+submit_pressed = False
 
 def increment():
-    dictionary_index += 1
-    return dictionary_index
-
+    global dictionary_index
+        dictionary_index += 1
+            return dictionary_index
 
 def decrement():
-    dictionary_index -= 1
-    return dictionary_index
+    global dictionary_index
+    if dictionary_index > 1:
+        dictionary_index -= 1
+        return dictionary_index
+
+def submit():
+    global submit_pressed
+    submit_pressed = True
+
+while True:
+    if GPIO.input(17) == GPIO.LOW:
+        increment()
+    elif GPIO.input(27) == GPIO.LOW:
+        decrement()
+    elif GPIO.input(22) == GPIO.LOW:
+        submit()
+        break
+    time.sleep(0.1)
